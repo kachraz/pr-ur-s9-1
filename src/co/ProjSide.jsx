98cde1a11bd583efdfa2cb1cdb9fe9542cfa2623
kachraz <kachraz@tuta.io>
2024-09-10 16:02:06 +0530

    add

--- src/co/ProjSide.jsx
+++ src/co/ProjSide.jsx
@@ -3,9 +3,11 @@
 export default function ProjectSidebar() {
   return (
     <aside className=" w-1/3 px-8 py-16 bg-emerald-950 text-emerald-300 md:w-72 rounded-r-xl">
-      <h2>YourPanty</h2>
+      <h2 className="mb-8 font-bold uppercase md:text-xl ">YourPanty</h2>
       <div>
-        <button>+AddPanty</button>
+        <button className="px-4 py-2 text-xs md:text-base rounded-md bg-indigo-900 text-indigo-400 hover:text-indigo-100 hover:bg-indigo-600">
+          +AddPanty
+        </button>
       </div>
       <ul></ul>
     </aside>
