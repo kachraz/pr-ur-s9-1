b82d1e843358ad9f42f9479e65ea623a18c8571d
kachraz <kachraz@tuta.io>
2024-09-10 15:52:09 +0530

    getSideBarReady

---
+++ src/co/ProjSide.jsx
@@ -0,0 +1,13 @@
+// Code for adding the side bar
+
+export default function ProjectSidebar() {
+  return (
+    <aside className=" w-1/3 px-8 py-16 bg-emerald-950 text-emerald-300 md:w-72 rounded-r-xl">
+      <h2>YourPanty</h2>
+      <div>
+        <button>+AddPanty</button>
+      </div>
+      <ul></ul>
+    </aside>
+  );
+}
