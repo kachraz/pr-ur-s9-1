7e83ccd61a72d693e1c1213861a8a45f969f8f5b
kachraz <kachraz@tuta.io>
2024-09-10 18:25:23 +0530

    add

--- src/co/NewProj.jsx
+++ src/co/NewProj.jsx
@@ -6,7 +6,9 @@ export default function NewProject() {
     <div className="w-[35rem] mt-16">
       <menu className="flex items-center justify-end gap-4 my-4">
         <li>
-          <button className="text-emerald-900 hover:text-black ">Cancel</button>
+          <button className="text-emerald-300 hover:bg-cyan-400 hover:text-rose-900 bg-indigo-700 px-6 py-2 rounded-md ">
+            Cancel
+          </button>
         </li>
         <li>
           <button className=" px-6 py-2 rounded-md bg-stone-800 text-emerald-50 hover:bg-emerald-950">
